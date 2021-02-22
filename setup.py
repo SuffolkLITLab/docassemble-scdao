4ecff8775e9dc763721e3554508c2870d47228b7
Anonymous User <60627397+shanacote@users.noreply.github.com>
2021-02-22 11:54:17 -0500

    added scdao code rather than test code

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
 setup(name='docassemble.scdao',
       version='0.0.1',
       description=('A docassemble extension.'),
-      long_description='# docassemble.scdao\n\nA docassemble extension.\n\n## Author\n\nscote4@su.suffolk.edu\n\n',
+      long_description='# docassemble.scdao\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nscote4@su.suffolk.edu\r\n\r\n',
       long_description_content_type='text/markdown',
       author='Shana Cote',
       author_email='scote4@su.suffolk.edu',
