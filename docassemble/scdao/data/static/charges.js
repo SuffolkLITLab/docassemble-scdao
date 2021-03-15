912350d96118b1d0fe5320e25cac60054e936a11
Anonymous User <60627397+shanacote@users.noreply.github.com>
2021-03-15 13:12:32 -0400

    charges tag option

---
+++ docassemble/scdao/data/static/charges.js
@@ -0,0 +1,13 @@
+var dataSource = [
+    {id: 1, firstName:'Tim', lastName:'Cook'},
+    {id: 2, firstName:'Eric', lastName:'Baker'},
+    {id: 3, firstName:'Victor', lastName:'Brown'},
+    {id: 4, firstName:'Lisa', lastName:'White'},
+    {id: 5, firstName:'Oliver', lastName:'Bull'},
+    {id: 6, firstName:'Zade', lastName:'Stock'},
+    {id: 7, firstName:'David', lastName:'Reed'},
+    {id: 8, firstName:'George', lastName:'Hand'},
+    {id: 9, firstName:'Tony', lastName:'Well'},
+    {id: 10, firstName:'Bruce', lastName:'Wayne'},
+];
+    
