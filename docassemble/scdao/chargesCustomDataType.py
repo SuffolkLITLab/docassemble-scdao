2ee0a8413a8a3a87fee7c0cbc00ca32dd82857b2
Joelle Ataya <70152267+joellea@users.noreply.github.com>
2021-04-12 11:54:45 -0400

    most recent

--- docassemble/scdao/chargesCustomDataType.py
+++ docassemble/scdao/chargesCustomDataType.py
@@ -5,52 +5,23 @@ class Charges(CustomDataType):
     name = 'charges'
     container_class = 'da-charges-container'
     input_class = 'magicsearch multi'
-    #input_type = 'charges'
     javascript = """\
-    var dataSource;
-    if(dataSource){
-        $(document).on('daPageLoad', function(){
-            $(".magicsearch").magicsearch({
-                dataSource: dataSource,
-                multiple: true,
-                fields: ["firstName","lastName"],
-                multiField: "firstName",
-                hidden:true,
-                id:"id",
-                format:"%firstName% · %lastName%"
-            });
-        });
-        $.validator.addMethod('charges', function(value, element, params){
-            return true;
-        });
-    };
+    $("#basic").magicsearch({
+    dataSource: dataSource,
+    multiple: true,
+    fields: ["firstName","lastName"],
+    multiField: "firstName",
+    id:"id",
+    format:"%firstName% · %lastName%"});
     """
 
-    is_object = True
-    jq_rule = 'Charges'
-
+    jq_rule = 'charges'
     jq_message = 'You need to enter the charges.'
 
     @classmethod
     def validate(cls, item):
+        #item = str(item).strip()
+        #m = re.search(r'^[0-9]{3}-?[0-9]{2}-?[0-9]{4}$', item)
+        #if item == '' or m:
         return True
-
-    @classmethod
-    def transform(cls, item):
-        dataSource = [
-            {'firstName':'Tim', 'lastName':'Cook'},
-            {'firstName':'Eric', 'lastName':'Baker'},
-            {'firstName':'Victor', 'lastName':'Brown'},
-            {'firstName':'Lisa', 'lastName':'White'},
-            {'firstName':'Oliver', 'lastName':'Bull'},
-            {'firstName':'Zade', 'lastName':'Stock'},
-            {'firstName':'David', 'lastName':'Reed'},
-            {'firstName':'George', 'lastName':'Hand'},
-            {'firstName':'Tony', 'lastName':'Well'},
-            {'firstName':'Bruce', 'lastName':'Wayne'},
-        ]
-        returnValue = []
-        for selection in item.split(","):
-            ds=dataSource[int(selection)+1]
-            returnValue.append(ds['firstName']+' '+ds['lastName'])
-        return returnValue
+        #raise DAValidationError("A charge needs to be found in the database")
